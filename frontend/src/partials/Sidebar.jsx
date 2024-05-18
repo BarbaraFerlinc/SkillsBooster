4214f90f400bf25100d3d081608f91174a67a1c6
KLARA KIRBIS <klara.kirbis@student.um.si>
2024-05-18 16:57:25 +0200

    popravki

--- frontend/src/partials/Sidebar.jsx
+++ frontend/src/partials/Sidebar.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import space from "../images/space-svgrepo-com.png";
 
-
+import {Domains} from "../Data.jsx";
+import SeznamDomen from "../pages/Domene/SeznamDomen.jsx";
+import Domena from "../pages/Domene/Domena.jsx"; // Import the SeznamDomen component
 
 function Sidebar({ sidebarOpen, setSidebarOpen }) {
   const location = useLocation();
@@ -44,116 +46,100 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
     }
   }, [sidebarExpanded]);
 
+  // Function to handle navigation to specific domain
+  const navigateToDomain = (domainId) => {
+    // Perform navigation to the specific domain using React Router
+  };
+
   return (
-    <div>
-      {/* Sidebar backdrop (mobile only) */}
-      <div
-        className={`fixed inset-0 bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
-          sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-        }`}
-        aria-hidden="true"
-      ></div>
+      <div>
+        {/* Sidebar backdrop (mobile only) */}
+        <div
+            className={`fixed inset-0 bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
+                sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+            }`}
+            aria-hidden="true"
+        ></div>
 
-      {/* Sidebar */}
-      <div
-        id="sidebar"
-        ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
-          sidebarOpen ? 'translate-x-0' : '-translate-x-64'
-        }`}
-      >
-        {/* Sidebar header */}
-        <div className="flex justify-between mb-10 pr-3 sm:px-2">
-          {/* Close button */}
-          <button
-              ref={trigger}
-              className="lg:hidden text-slate-500 hover:text-slate-400"
-              onClick={() => setSidebarOpen(!sidebarOpen)}
-              aria-controls="sidebar"
-              aria-expanded={sidebarOpen}
-          >
-            <span className="sr-only">Close sidebar</span>
-            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z"/>
-            </svg>
-          </button>
-          {/* Logo */}
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <NavLink end to="/" className="block" >
-                <img src={space} alt="Icon" className="w-12 h-12 mr-2" title={"Home"}/>
+        {/* Sidebar */}
+        <div
+            id="sidebar"
+            ref={sidebar}
+            className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
+                sidebarOpen ? 'translate-x-0' : '-translate-x-64'
+            }`}
+        >
+          {/* Sidebar header */}
+          <div className="flex justify-between mb-10 pr-3 sm:px-2">
+            {/* Close button */}
+            <button
+                ref={trigger}
+                className="lg:hidden text-slate-500 hover:text-slate-400"
+                onClick={() => setSidebarOpen(!sidebarOpen)}
+                aria-controls="sidebar"
+                aria-expanded={sidebarOpen}
+            >
+              <span className="sr-only">Close sidebar</span>
+              <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z"/>
+              </svg>
+            </button>
+            {/* Logo */}
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <NavLink end to="/" className="block" >
+                  <img src={space} alt="Icon" className="w-12 h-12 mr-2" title={"Home"}/>
 
-              </NavLink>
+                </NavLink>
+              </div>
             </div>
+
           </div>
 
-        </div>
-
-        {/* Links */}
-        <div className="space-y-8">
-          {/* More group */}
-          <div>
-            <h3 className="text-xs uppercase text-slate-500 font-semibold pl-3">
+          {/* Links */}
+          <div className="space-y-8">
+            {/* More group */}
+            <div>
+              <h3 className="text-xs uppercase text-slate-500 font-semibold pl-3">
               <span className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6"
                     aria-hidden="true">
                 •••
               </span>
-              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">Domene</span>
-            </h3>
-            <ul className="mt-3">
-              {/* Add Domain */}
-              <li>
-                <a
-                    href="#0"
-                    className={`block text-slate-200 truncate transition duration-150 ${
-                        pathname === '/' || pathname.includes('dashboard') ? 'hover:text-slate-200' : 'hover:text-white'
-                    }`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      sidebarExpanded ? handleClick() : setSidebarExpanded(true);
-                    }}
-                >
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <NavLink to="/domena"
-                               className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                        Domena
-                      </NavLink>
-                    </div>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <NavLink to="/addDomena"
-                               className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                        Dodaj Domeno
-                      </NavLink>
-                    </div>
-                  </div>
+                <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">Domene</span>
+              </h3>
+              <ul className="mt-3">
+                {/* Render SeznamDomen component */}
+                <li>
+                  <SeznamDomen domains={Domains}/>
+                </li>
+              </ul>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <NavLink to="/domena"
+                           className="text-sm font-medium ml-3 text-white lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                    Domena
+                  </NavLink>
+                </div>
+              </div>
 
 
-                </a>
-              </li>
-
-
-            </ul>
-
+            </div>
           </div>
-        </div>
 
-        {/* Expand / collapse button */}
-        <div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
-          <div className="px-3 py-2">
-            <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
-              <span className="sr-only">Expand / collapse sidebar</span>
-              <svg className="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
-                <path className="text-slate-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z"/>
-                <path className="text-slate-600" d="M3 23H1V1h2z"/>
-              </svg>
-            </button>
+          {/* Expand / collapse button */}
+          <div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
+            <div className="px-3 py-2">
+              <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
+                <span className="sr-only">Expand / collapse sidebar</span>
+                <svg className="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
+                  <path className="text-slate-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z"/>
+                  <path className="text-slate-600" d="M3 23H1V1h2z"/>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
