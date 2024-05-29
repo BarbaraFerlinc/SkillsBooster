ae27c4ca6b94142b511da3d20489c44363eace7a
KLARA KIRBIS <klara.kirbis@student.um.si>
2024-05-29 16:36:04 +0200

    nic

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+const {getFirestore}= require('firebase-admin/firestore')
 
 import dotenv from 'dotenv';
 
@@ -11,16 +11,21 @@ dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: process.env.EXPRESS_APP_API_KEY,
+  authDomain: process.env.EXPRESS_APP_AUTH_DOMAIN,
+  projectId: process.env.EXPRESS_APP_PROJECT_ID,
+  storageBucket: process.env.EXPRESS_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPRESS_APP_MESSAGING_SENDER_ID,
+  appId: process.env.EXPRESS_APP_APP_ID
 };
+
+const db= getFirestore()
+module.exports={db}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export default app;
+
+
